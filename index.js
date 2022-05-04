4d84f643ba0dffb7eed1b1a35f9a014de89d421e
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2022-05-05 02:27:00 +0600

    Mongodb connected with server with .env file

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const app = express();
-const port =process.env.PORT || 5000
+const port = process.env.PORT || 5000
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+//database connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h5pfr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect()
+        const itemsCollection = client.db('eElectronics').collection('items');
+
+        app.get('/items', async(req, res) => {
+            const query = {}
+            const cursor = itemsCollection.find(query)
+            const items = await cursor.toArray()
+            res.send(items)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+
+
 
 app.get('/', (req, res) => {
     res.send('eElectronics runing')
@@ -14,4 +40,4 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log(`eElectronics started on ${port}`)
-  })
+})
